176d9a5247b47f7304b38160ea2fe4bc074959f3
Sachin Jadhav <ipssachin@gmail.com>
2024-02-11 19:38:55 +0530

    comments

--- hello.c
+++ hello.c
@@ -1,10 +1,16 @@
 // Header liberay file
+
 #include<stdio.h>
+
+/*main(). This is called a function. Any code inside its curly brackets {} will be executed.*/
 
 int  main(){
 
+    /*printf() is a function used to output/print text to the screen. In our example, it will output "Hello World!".*/
+
     printf("Hello World");
 
+    /*return 0 ends the main() function.*/
     return 0;
 }
 
