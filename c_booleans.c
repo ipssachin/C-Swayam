d3d36ff87de38410e0e38d3800b6c104c82f57f6
Sachin Jadhav <ipssachin@gmail.com>
2024-02-12 21:02:24 +0530

    boolian_completed

---
+++ c_booleans.c
@@ -0,0 +1,13 @@
+#include<stdbool.h>
+#include<stdio.h>
+
+int main(){
+    /* Bool is not built in data type hence it needs to be imported*/
+    bool isCodingFun = true;
+    bool isSunCool = false;
+
+    printf("%d\n", isCodingFun);
+    printf("%d \n", isSunCool);
+
+    return 0;
+}
