419c07bfb92b1cea06a1f50ea52b53845e47457b
Sachin Jadhav <ipssachin@gmail.com>
2024-02-13 20:56:59 +0530

    update c_booleans

--- c_booleans.c
+++ c_booleans.c
@@ -9,5 +9,16 @@ int main(){
     printf("%d\n", isCodingFun);
     printf("%d \n", isSunCool);
 
+
+    printf("%d\n", 10 == 10); // Returns 1 (true), because 10 is equal to 10
+    printf("%d\n", 10 == 15); // Returns 0 (false), because 10 is not equal to 15
+    printf("%d\n", 5 == 55);  // Returns 0 (false) because 5 is not equal to 55
+
+    bool isHamburgerTasty = true;
+    bool isPizzaTasty = true;
+
+    // Find out if both hamburger and pizza is tasty
+    printf("%d\n", isHamburgerTasty == isPizzaTasty); 
+
     return 0;
 }
