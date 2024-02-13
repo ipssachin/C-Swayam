265a7599ebaad4ff3f8bda390c35b5e7fa4fceca
Sachin Jadhav <ipssachin@gmail.com>
2024-02-13 21:11:00 +0530

    real life example 4 done

---
+++ ex4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+int main(){
+
+    int myAge = 5;
+    int votingAge = 18;
+
+    if (myAge >= votingAge)
+    {
+        printf("You are old enough to vote!!");
+    } else{
+        printf("You are not old enough to vote!!");
+    }
+
+    return 0;
+    
+
+}
